33a3cac805ac8633d3cceed6e71a7d382d59a976
shamim19372 <shamimislam192729@gmail.com>
2025-10-28 00:11:56 +0600

    Update libfix.sh

--- libfix.sh
+++ libfix.sh
@@ -1,10 +1,112 @@
 # YOU CAN SKIP ALL OF THESE ITS OPTIONAL!
 # Optional login methods
-APPSTATE='your cooki'  
-EMAIL='shamimrahman@gamil.com'  
-PASSWORD='*******'  
-
-# Bot settings  
-PREFIX='YOUR BOT PREFIX e.g: -'  
-sitekey='YOUR RECAPTCHA SITEKEY Skip this if you already configured the Shamim.json'  
-PORT='YOUR PORT e.g. 25645'  
+[
+    {
+        "key": "datr",
+        "value": "doX_aBxY1nBRaJsdWh_UWKgj",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.113Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "sb",
+        "value": "d4X_aJhPf-qq4gpqg9lYNmDR",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61578723531794",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "xs",
+        "value": "37%3AglE2jGElk-NXDA%3A2%3A1761577237%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "locale",
+        "value": "en_US",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "pas",
+        "value": "61578723531794%3A00ltKTgIVE",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B674x360x2",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "fr",
+        "value": "0jZls8zBW0UIvXgNa.AWddRFRA1eiJjMtAHyEiGS7bf_NFtNIM2nzEIippbjC4fQqE3VU.Bo_4V3..AAA.0.0.Bo_7TU.AWdz_7Wu4i-7RUxnxI2e8iYOtBc",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "101739758%3BT%3A29359807",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2965%3Btimestamp%3A1761588436",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-27T18:09:28.114Z",
+        "lastAccessed": "2025-10-27T18:09:28.114Z"
+    }
+]
